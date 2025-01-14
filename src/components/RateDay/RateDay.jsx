4386c94427f29e4869797bf4f3d1bba109cdfca4
Fazila Baksh <fazilabak@gmail.com>
2025-01-14 14:31:11 -0500

    created and styled rate your day component

---
+++ src/components/RateDay/RateDay.jsx
@@ -0,0 +1,34 @@
+import "./RateDay.scss";
+import happy from "../../assets/icons/happy.svg";
+import neutral from "../../assets/icons/neutral.svg";
+import sad from "../../assets/icons/sad.svg";
+
+function RateDay() {
+  return (
+    <section className="rate">
+      <h2 className="rate__title">rate your day</h2>
+      <div className="rate__card">
+        <div className="rate__icon-container">
+          <div className="rate__icon">
+            <img className="rate__img" src={happy} />
+            <p className="rate__info">great</p>
+          </div>
+          <div className="rate__icon">
+            <img className="rate__img" src={neutral} />
+            <p className="rate__info">fine</p>
+          </div>
+          <div className="rate__icon">
+            <img className="rate__img" src={sad} />
+            <p className="rate__info">gloomy</p>
+          </div>
+        </div>
+        <form className="rate__form">
+          <label className="rate__form-label">describe your day</label>
+          <input className="rate__form-input" type="text" />
+        </form>
+      </div>
+    </section>
+  );
+}
+
+export default RateDay;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import DailyHabitsList from "../../components/DailyHabitsList/DailyHabitsList";
 import Banner from "../../components/Banner/Banner";
+import RateDay from "../../components/RateDay/RateDay";
 
 function HomePage() {
   return (
     <>
       <Banner />
       <DailyHabitsList />
+      <RateDay />
     </>
   );
 }
