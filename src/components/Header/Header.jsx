8580143f9a811cf27da8eb20de0ec98f2511161a
Fazila Baksh <fazilabak@gmail.com>
2025-01-13 20:07:31 -0500

    made style changes to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,18 @@
 import "./Header.scss";
-import menu from "../../assets/icons/menu.svg";
+import { NavLink } from "react-router";
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">inertia</h1>
-      <img className="header__icon" src={menu} />
+      <nav className="header__nav">
+        <NavLink to="/" className="header__nav-link">
+          daily
+        </NavLink>
+        <NavLink to="/weekly" className="header__nav-link">
+          weekly
+        </NavLink>
+      </nav>
     </header>
   );
 }
